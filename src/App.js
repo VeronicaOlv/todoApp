be902fdb2f992c39226bc7f6aa4444376e562db7
Veronica Olv <veronica.olvera.cruz@gmail.com>
2021-03-29 19:56:47 -0600

    Cambio de colores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import InputBar from './InputBar';
+import Tareas from './Tareas';
 
 function App() {
   return (
     <div className="App">
       <InputBar />
+      <Tareas />
     </div>
   );
 }
---
+++ src/Tareas.js
@@ -0,0 +1,17 @@
+import './tareas.css';
+
+const Tareas = () => {
+    return (
+        <div>
+          <div>Tarea
+          <input 
+            className="btn-e"
+            type="button"
+            value="Guardar"
+    ></input>
+    </div>
+        </div>
+    )
+}
+
+export default Tareas;
